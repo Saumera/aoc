99a0c95e4fbe3fddfb83044899ddbc5618b288db
Nick Coronado <nick@mavenmachines.com>
2021-12-05 13:00:48 -0500

    day 5 - updated to using Sets

--- 2021/solutions/day5.js
+++ 2021/solutions/day5.js
@@ -2,47 +2,33 @@ const { getProblem } = require('../helpers.js');
 
 const lines = getProblem(5);
 const segments = lines.map(line => line.split(" -> ").map(end => end.split(',').map(Number)))
+const SIZE = Math.max(...segments.flat(2)) + 1;
 
-const getStartGrid = segs => {
-  const max = Math.max(...segs.flat(2));
-  return [ ...new Array(max + 1) ].map(_ => {
-    return [ ...new Array(max + 1) ].map(_ => 0);
+const getInc = (s, e) => (e - s) !== 0 ? (e - s) / Math.abs(e - s) : 0;
+const interpolate = (s, e, size) => [...Array(size).keys()].map(i => s + (i * getInc(s, e)));
+const markSegment = (seg, seen, multiples) => {
+  const size = Math.max(Math.abs(seg[0][0] - seg[1][0]), Math.abs(seg[0][1] - seg[1][1])) + 1;
+  const xValues = interpolate(seg[0][0], seg[1][0], size);
+  const yValues = interpolate(seg[0][1], seg[1][1], size);
+  xValues.map((x, i) => x * SIZE + yValues[i]).forEach(val => {
+    if (seen.has(val)) multiples.add(val);
+    seen.add(val);
   })
 }
 
-const markGrid = (grid, x, y) => {
-  grid[y][x]++;
-  return grid
-}
-const markSegment = (grid, seg) => {
-  const diffX = Math.abs(seg[0][0] - seg[1][0]);
-  const diffY = Math.abs(seg[0][1] - seg[1][1]);
-  const slopeX = diffX > 0 ? (seg[1][0] - seg[0][0]) / diffX : 0;
-  const slopeY = diffY > 0 ? (seg[1][1] - seg[0][1]) / diffY : 0;
-  const coords = [ ...new Array(Math.max(diffX, diffY) + 1) ].map((_, i) => [
-    seg[0][0] + (i * slopeX),
-    seg[0][1] + (i * slopeY)
-  ])
-  return coords.reduce((g, c) => markGrid(g, c[0], c[1]), grid);
-}
-
-const getOverlaps = (startGrid, segments) => {
-  const markSegments = (grid, remainingSegments) => {
-    if (!remainingSegments.length) return grid;
-    const [seg, ...remaining] = remainingSegments;
-    return markSegments(markSegment(grid, seg), remaining);
-  }
-
-  const finalGrid = markSegments(startGrid, segments);
-  return finalGrid.flat().filter(c => c >= 2).length;
+const getOverlaps = allSegments => {
+  const seen = new Set();
+  const multiples = new Set();
+  allSegments.forEach(seg => markSegment(seg, seen, multiples))
+  return multiples.size;
 }
 
 const part1 = (() => {
   const filtered = segments.filter(([[x1, y1], [x2, y2]]) => x1 === x2 || y1 === y2);
-  return getOverlaps(getStartGrid(filtered), filtered);
+  return getOverlaps(filtered);
 })()
 
-const part2 = getOverlaps(getStartGrid(segments), segments);
+const part2 = getOverlaps(segments);
 
 console.log(part1);
 console.log(part2);
