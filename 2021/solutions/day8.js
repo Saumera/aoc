ec48aeab8e38df3527d104139484759c5ed2e303
Nick Coronado <nick@mavenmachines.com>
2021-12-08 21:46:59 -0500

    day 8 - new algorithm

--- 2021/solutions/day8.js
+++ 2021/solutions/day8.js
@@ -1,64 +1,46 @@
 const { getProblem } = require('../helpers.js');
 
+const parseDigit = d => d.split('').sort().join('')
 const parse = str => str.split(/\n/g).filter(Boolean).map(line => {
-  return line.split(' | ').map(sets => sets.split(' ').filter(Boolean).map(num => new Set(num.split(''))))
+  return line.split(' | ').map(sets => sets.split(' ').filter(Boolean).map(parseDigit));
 })
 const lines = getProblem(8, parse);
 
 const part1 = (() => {
   const outputs = lines.map(([_, out]) => out);
   return outputs.reduce((count, digits) => {
-    return count + digits.filter(d => [2, 3, 4, 7].includes(d.size)).length
+    return count + digits.filter(d => [2, 3, 4, 7].includes(d.length)).length
   }, 0)
 })()
 
-const diff = (s1, s2) => new Set([
-  ...[...s1].filter(c => !s2.has(c)),
-  ...[...s2].filter(c => !s1.has(c)),
-])
-
-const NUMBERS = {
-  'abcefg': 0,
-  'cf': 1,
-  'acdeg': 2,
-  'acdfg': 3,
-  'bcdf': 4,
-  'abdfg': 5,
-  'abdefg': 6,
-  'acf': 7,
-  'abcdefg': 8,
-  'abcdfg': 9,
-}
-const getMissingChar = set => 'abcdefg'.split('').find(c => !set.has(c));
+const contains = (str, sub) => sub.split('').every(c => str.includes(c))
 const part2 = (() => {
-  const getNumberLookup = (numSets) => {
-    const map = 'abcdefg'.split('').reduce((o, c) => ({ ...o, [c]: "" }), {});
-    // First we find a 1 and a 4
-    const one = numSets.find(s => s.size === 2);
-    const four = numSets.find(s => s.size === 4);
-    // Then find a 7 and figure out the top line by comparing it to the one.
-    map[[...numSets.find(s => s.size === 3)].find(c => !one.has(c))] = 'a';
+  const getLookup = (digits) => {
+    const numStrings = Array(10).fill('');
+    // first we get the easy ones
+    numStrings[1] = digits.find(d => d.length === 2);
+    numStrings[7] = digits.find(d => d.length === 3);
+    numStrings[4] = digits.find(d => d.length === 4);
+    numStrings[8] = digits.find(d => d.length === 7);
 
-    const fourDiff = diff(four, one);
-    const zero = numSets.find(s => s.size === 6 && fourDiff.has(getMissingChar(s)));
-    const missingCharZero = getMissingChar(zero);
-    fourDiff.forEach(c => map[c] = c === missingCharZero ? 'd' : 'b')
+    // let's look through digits of length 6: 0, 6, 9
+    const sixes = digits.filter(d => d.length === 6);
+    numStrings[9] = sixes.find(d => contains(d, numStrings[4]));
+    numStrings[6] = sixes.find(d => !contains(d, numStrings[1]));
+    numStrings[0] = sixes.find(d => d !== numStrings[9] && d !== numStrings[6]);
 
-    const six = numSets.find(s => s.size === 6 && one.has(getMissingChar(s)));
-    const missingCharSix = getMissingChar(six);
-    one.forEach(c => map[c] = c === missingCharSix ? 'c' : 'f')
+    // Now let's look through digits of length 5: 2, 3, 5
+    const fives = digits.filter(d => d.length === 5);
+    numStrings[3] = fives.find(d => contains(d, numStrings[1]));
+    numStrings[5] = fives.find(d => contains(numStrings[6], d));
+    numStrings[2] = fives.find(d => d !== numStrings[3] && d !== numStrings[5]);
 
-    const remaining = new Set(Object.keys(map).filter(k => !map[k]));
-    const nine = numSets.find(s => s.size === 6 && remaining.has(getMissingChar(s)));
-    const missingCharNine = getMissingChar(nine);
-    remaining.forEach(c => map[c] = c === missingCharNine ? 'e' : 'g')
-
-    return numSet => NUMBERS[[...numSet].map(c => map[c]).sort().join('')]
+    return numStrings.reduce((o, s, i) => ({ ...o, [s]: i }), {})
   }
 
   const getNumber = ([input, output]) => {
-    const lookup = getNumberLookup(input);
-    return output.reduce((n, s) => (n * 10) + lookup(s), 0)
+    const lookup = getLookup(input);
+    return output.reduce((n, s) => (n * 10) + lookup[s], 0)
   }
 
   return lines.reduce((total, line) => total + getNumber(line), 0);
